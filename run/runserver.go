17c3053771e9328169dad8a83b7028e2bc7c6832
jordan9001 <jordan9001@gmail.com>
2016-10-08 13:37:38 -0600

    Added argument for commandfile

--- run/runserver.go
+++ run/runserver.go
@@ -3,9 +3,14 @@ package main
 import (
 	"github.com/jordan9001/rush_n_crush"
 	"net/http"
+	"os"
 )
 
 func main() {
 	go http.ListenAndServe(":8080", http.FileServer(http.Dir("/var/www/site")))
-	rush_n_crush.StartServer("/", ":12345", "./run/startup2.cmd")
+	var startup_file string
+	if len(os.Args) > 1 {
+		startup_file = os.Args[1]
+	}
+	rush_n_crush.StartServer("/", ":12345", startup_file)
 }
