738aba30aab0d50f537adcdb272bcbc635a916a9
jordan9001 <jordan9001@gmail.com>
2016-11-21 17:24:25 -0700

    Added click to move

--- run/site/js/rnc.js
+++ run/site/js/rnc.js
@@ -27,6 +27,10 @@ function RushNCrush(url, canvas_id) {
 	this.powerups = [];
 	this.targets = [];
 
+	this.setpath = [];
+	this.shootmode = false;
+	this.actionqueue = [];
+
 	this.animating = false;
 	this.player_ani_queue = [];
 
@@ -65,12 +69,16 @@ function RushNCrush(url, canvas_id) {
 		var ret_code = false;
 		var panspeed = 24;
 		if (evt.keyCode == 65) {
+			that.actionqueue = [];
 			that.move_player(-1,0);
 		} else if (evt.keyCode == 68) {
+			that.actionqueue = [];
 			that.move_player(1,0);
 		} else if (evt.keyCode == 87) {
+			that.actionqueue = [];
 			that.move_player(0,-1);
 		} else if (evt.keyCode == 83) {
+			that.actionqueue = [];
 			that.move_player(0,1);
 		} else if (evt.keyCode == 37) {
 			that.focux -= (1/that.zoom) * panspeed;
@@ -101,10 +109,13 @@ function RushNCrush(url, canvas_id) {
 			that.playerlock = false;
 			that.draw(true);
 		} else if (evt.keyCode == 32) {
+			that.actionqueue = [];
 			that.next_player();
 		} else if (evt.keyCode == 13) {
+			that.actionqueue = [];
 			that.end_turn();
 		} else if (evt.keyCode <= 57 && evt.keyCode >= 49) {
+			that.actionqueue = [];
 			that.choose_weapon(evt.keyCode - 49);
 		} else {
 			ret_code = true;
@@ -148,6 +159,9 @@ RushNCrush.prototype.handle_scroll = function(px, py, scrolly) {
 }
 
 RushNCrush.prototype.handle_point = function(px, py) {
+	if (this.animating) {
+		return;
+	}
 	coord = that.px2coord(px, py);
 	// aim, if it is your turn and you have a guy selected, and he has turns
 	if (this.user_turn != undefined && this.user_turn == this.userid && this.player_index >= 0) {
@@ -156,39 +170,120 @@ RushNCrush.prototype.handle_point = function(px, py) {
 		var ang = 180 * Math.atan2(coord[1] - ply, coord[0] - plx) / Math.PI;
 		this.players[this.player_index].dir = Math.floor(ang);
 		this.draw(false);
-	}
-	// change cursor based on things
-	var aimcursor = true;
-	for (var i=0; i<this.clickable.length; i++) {
-		if (px > this.clickable[i].x && py > this.clickable[i].y && px < this.clickable[i].x + this.clickable[i].w && py < this.clickable[i].y + this.clickable[i].h) {
-			aimcursor = false;
-			break;
+
+		var aimcursor = true;
+		for (var i=0; i<this.clickable.length; i++) {
+			if (px > this.clickable[i].x && py > this.clickable[i].y && px < this.clickable[i].x + this.clickable[i].w && py < this.clickable[i].y + this.clickable[i].h) {
+				aimcursor = false;
+				break;
+			}
 		}
-	}
-	if (aimcursor && (coord[0] < 0 || coord[1] < 0 || coord[0] > this.mapw || coord[1] > this.maph)) {
-		aimcursor = false;
-	}
-	
-	if (aimcursor && document.body.style.cursor != "crosshair") {
-		document.body.style.cursor = "crosshair";
-	} else if (!aimcursor && document.body.style.cursor != "default") {
-		document.body.style.cursor = "default";
+		if (aimcursor && (coord[0] < 0 || coord[1] < 0 || coord[0] > this.mapw || coord[1] > this.maph)) {
+			aimcursor = false;
+		}
+		
+		if (aimcursor) {
+			// we are pointing at the game map
+			// if we are in shoot mode:
+			if (this.shootmode) {
+				if (document.body.style.cursor != "crosshair") {
+					document.body.style.cursor = "crosshair";
+				}
+			} else {
+				// if we are in move mode:
+				// pathfinding
+				this.setpath = this.pathfind(this.players[this.player_index].pos.x, this.players[this.player_index].pos.y, coord[0], coord[1], this.players[this.player_index].moves);
+			}
+
+		} else {
+			if (document.body.style.cursor != "default") {
+				document.body.style.cursor = "default";
+			}
+			this.setpath = [];
+		}
+	} else {
+		if (document.body.style.cursor != "default") {
+			document.body.style.cursor = "default";
+		}
+		this.setpath = [];
 	}
 };
 
 RushNCrush.prototype.handle_click = function(px, py) {
+	if (this.animating) {
+		return;
+	}
+	// clear action queue
+	if (this.actionqueue.length > 0) {
+		this.actionqueue = [];
+		console.log("Cleared action queue because of click");
+	}
 	// check if we are over any of our hotspots
 	for (var i=0; i<this.clickable.length; i++) {
+		this.draw(false);
 		if (px > this.clickable[i].x && py > this.clickable[i].y && px < this.clickable[i].x + this.clickable[i].w && py < this.clickable[i].y + this.clickable[i].h) {
 			this.clickable[i].click();
 			return;
 		}
 	}
-	// check bounds	
-	coord = that.px2coord(px, py);
-	if (coord[0] >= 0 && coord[1] >= 0 && coord[0] < this.mapw && coord[1] < this.maph) {
-		this.fire();
+	if (this.shootmode) {
+		// check bounds	
+		coord = this.px2coord(px, py);
+		if (coord[0] >= 0 && coord[1] >= 0 && coord[0] < this.mapw && coord[1] < this.maph) {
+			this.fire();
+		}
+	} else if (this.setpath.length > 0) {
+		// add to action queue
+		for (var i=this.setpath.length-2; i>= 0; i--) {
+			var that = this;
+			var action = {};
+			action.call = function() {
+				if (this.condition()) {
+					this.act();
+					return true;
+				}
+				return false;
+			};
+			action.expectplayerid = this.players[this.player_index].id;
+			action.expectposition = this.setpath[i+1];
+			action.dx = this.setpath[i].x - this.setpath[i+1].x;
+			action.dy = this.setpath[i].y - this.setpath[i+1].y;
+			action.condition = function() {
+				if (that.players[that.player_index].id !== this.expectplayerid) {
+					return false;
+				}
+				if (that.players[that.player_index].pos.x !== this.expectposition.x || that.players[that.player_index].pos.y !== this.expectposition.y) {
+					return false;
+				}
+				return true;
+			};
+			action.act = function() {
+				that.move_player(this.dx, this.dy);
+			};
+			this.actionqueue.push(action);
+		}
+		this.continue_actionqueue();
 	}
+	// remove shootmode
+	this.shootmode = false;
+	if (document.body.style.cursor != "default") {
+		document.body.style.cursor = "default";
+	}
+	this.setpath = [];
+}
+
+RushNCrush.prototype.continue_actionqueue = function() {
+	if (this.actionqueue.length === 0) {
+		return false;
+	}
+	var a = this.actionqueue.shift(1);
+	var ret = a.call();
+	if (!ret) {
+		console.log("action queue condition not met");
+		this.actionqueue = [];
+		return false;
+	}
+	return true;
 }
 
 RushNCrush.prototype.fire = function() {
@@ -218,8 +313,10 @@ RushNCrush.prototype.handle_message = function(message_type, data) {
 		// draw animations
 		// then draw everything
 		that = this;
-		this.run_animations(function() {
+		this.run_animations_oneatatime(function() {
 			that.draw(true);
+			// next action
+			that.continue_actionqueue();
 		});
 	}
 };
@@ -229,6 +326,7 @@ RushNCrush.prototype.choose_weapon = function(wi) {
 		if (this.players[this.player_index].weapons[wi].ammo != 0 && this.players[this.player_index].moves >= this.players[this.player_index].weapons[wi].move_cost) {
 			this.players[this.player_index].selected_weapon = wi;
 			this.draw(false);
+			this.shootmode = true;
 		}
 	}
 	
@@ -236,6 +334,7 @@ RushNCrush.prototype.choose_weapon = function(wi) {
 
 RushNCrush.prototype.end_turn = function() {
 	this.ws.send("end_turn:");
+	this.setpath = [];
 	console.log("sent end_turn");
 };
 
@@ -245,9 +344,9 @@ RushNCrush.prototype.send_start = function() {
 }
 
 RushNCrush.prototype.move_player = function(dx, dy) {
-	if (this.animating) {
+	/*if (this.animating) {
 		return;
-	}
+	}*/
 	if (this.player_index < 0) {
 		return;
 	}
@@ -272,6 +371,7 @@ RushNCrush.prototype.next_player = function() {
 			this.focux = this.players[this.player_index].pos.x + 0.5;
 			this.focuy = this.players[this.player_index].pos.y + 0.5;
 			this.playerlock = true;
+			this.setpath = [];
 			this.draw(false);
 			return;
 		}
@@ -285,6 +385,7 @@ RushNCrush.prototype.choose_player = function(id) {
 			this.focux = this.players[this.player_index].pos.x + 0.5;
 			this.focuy = this.players[this.player_index].pos.y + 0.5;
 			this.playerlock = true;
+			this.setpath = [];
 			this.draw(false);
 			return;
 		}
@@ -396,6 +497,11 @@ RushNCrush.prototype.update_game = function(data) {
 		this.ingame = has_players;
 		// if there are extras left over, add them
 		for (var i=0; i<u_p.length; i++) {
+			// saw a new player, clear the queue
+			if (this.actionqueue.length > 0) {
+				this.actionqueue = [];
+				console.log("Cleared action queue, because we saw an enemy");
+			}
 			this.players.push(u_p[i]);
 			if (u_p[i].owner == this.userid && this.player_index < 0) {
 				// focus on the new player
@@ -408,13 +514,13 @@ RushNCrush.prototype.update_game = function(data) {
 	}
 	// handle powerups
 	this.powerups = data.powerups;
-	console.log(this.powerups);
 
 	// handle hit tiles
 	var h_t = data.hit_tiles;
 	for (var i=0; i<h_t.length; i++) {
 		this.hit_animate(h_t[i].pos.x, h_t[i].pos.y, h_t[i].from_pos.x, h_t[i].from_pos.y, h_t[i].damage_type);
 	}
+
 	return true;
 };
 
@@ -468,8 +574,17 @@ RushNCrush.prototype.run_animations_oneatatime = function(callback) {
 			that.animating = false;
 			callback();
 			return;
-		}else if (that.player_ani_queue[0]() == false) {
-			that.player_ani_queue.shift();
+		}
+		var ani_group = that.player_ani_queue[0];
+		if (ani_group.length === 0) {
+			that.player_ani_queue.shift(1);
+		} else {
+			for (var i=0; i<ani_group.length; i++) {
+				if (ani_group[i]() == false) {
+					ani_group.splice(i,1);
+					i--;
+				}
+			}
 		}
 		window.requestAnimationFrame(animate);
 	}
@@ -506,7 +621,10 @@ RushNCrush.prototype.hit_animate = function(hitx,hity, fromx,fromy, type) {
 		anistep++;
 		return true;
 	}
-	this.player_ani_queue.push(draw_hit);
+	if (this.player_ani_queue.length === 0) {
+		this.player_ani_queue[0] = [];
+	}
+	this.player_ani_queue[this.player_ani_queue.length - 1].push(draw_hit);
 }
 
 RushNCrush.prototype.player_animate = function(p_index, sx,sy, x,y) {
@@ -539,7 +657,7 @@ RushNCrush.prototype.player_animate = function(p_index, sx,sy, x,y) {
 		}
 	}
 	// put this function into the queue
-	this.player_ani_queue.push(update_player);
+	this.player_ani_queue.push([update_player]);
 }
 
 // Turns an x,y coordinate to the pixel coordinate at the top left of the box
@@ -596,6 +714,11 @@ RushNCrush.prototype.draw = function(cast) {
 	// Draw powerups
 	for (var i=0; i<this.powerups.length; i++) {
 		this.draw_powerup(this.powerups[i]);
+	}
+
+	// Draw the path
+	if (!this.shootmode) {
+		this.draw_path(this.setpath);
 	}
 
 	// Draw the players
@@ -731,7 +854,7 @@ RushNCrush.prototype.draw_tile = function(tile_obj, x, y) {
 
 	// draw debug index
 	//this.ctx.fillStyle = "#000000";
-	//this.ctx.font="8px";
+	//this.ctx.font="6px";
 	//this.ctx.fillText(""+x+","+y, topl[0] + 3, topl[1] + (w/2));
 };
 
@@ -771,6 +894,26 @@ RushNCrush.prototype.draw_target = function(target) {
 	this.ctx.arc(center[0], center[1], this.zoom/6, 0, 2*Math.PI);
 	this.ctx.fill();
 };
+
+RushNCrush.prototype.draw_path = function(path) {
+	if (path == undefined || path.length === 0) {
+		return;
+	}
+	var teamcolor = this.get_client_color(this.userid);
+	this.ctx.strokeStyle = teamcolor;
+	this.ctx.lineWidth = this.zoom / 9;
+	this.ctx.beginPath();
+	for (var i=0; i<path.length; i++) {
+		var center = this.coord2px(path[i].x + 0.5, path[i].y + 0.5);
+		if (i===0) {
+			this.ctx.moveTo(center[0], center[1]);
+		} else {
+			this.ctx.lineTo(center[0], center[1]);
+		}
+	}
+	this.ctx.stroke();
+	
+}
 
 RushNCrush.prototype.draw_ui = function() {
 	var box_size = 12;
@@ -816,10 +959,9 @@ RushNCrush.prototype.draw_ui = function() {
 		this.ctx.fillRect(x, y, w, box_size);
 
 		var wi = this.players[this.player_index].selected_weapon
-		if (wi == undefined) {
+		if (!this.shootmode || wi == undefined) {
 			wi = 0;
-		}
-		if (i == wi) {
+		} else if (i == wi) {
 			this.ctx.strokeRect(x - pad/2,y - pad/2, w + pad, box_size + pad);
 		}
 
@@ -857,6 +999,96 @@ RushNCrush.prototype.draw_ui = function() {
 		this.clickable.push(clickarea);
 	}
 };
+
+RushNCrush.prototype.tile_moveable = function(mx,my) {
+	if (mx < 0 || mx >= this.mapw || my < 0 || my >= this.maph) {
+		return false;
+	}
+	tt = this.map[my][mx].tType;
+	if (tt <= 7 || tt == 14) { // a bit of a cheat, because 14 can we walked through if the target is shot
+		return false;
+	}
+	// check if anyone is there
+	for (var i=0; i<this.players.length; i++) {
+		if (this.players[i].pos.x == mx && this.players[i].pos.y == my) {
+			return false;
+		}
+	}
+	return true;
+}
+
+RushNCrush.prototype.pathfind = function(sx,sy, ex,ey, maxdepth) {
+	ex = Math.floor(ex);
+	ey = Math.floor(ey);
+	// check if we are trying to move to a taken spot
+	if (!this.tile_moveable(ex,ey)) {
+		return [];
+	}
+	var fmap = [[]];
+	var q = [];
+	fmap[sy] = [];
+	fmap[sy][sx] = {px:sx, py:sy}; // add to seen map
+	q.push({x:sx, y:sy, depth:0}); // add to queue
+	var reached = false;
+	while (q.length > 0) {
+		// check if we made it
+		if (q[0].x == ex && q[0].y == ey) {
+			reached = true;
+			break;
+		} else if (q[0].depth < maxdepth) {
+			// add neighbors
+			for (var dy=-1; dy<=1; dy++) {
+				for (var dx=-1; dx<=1; dx++) {
+					// no diagonal
+					if (dy !== 0 && dx !== 0) {
+						continue;
+					}
+					if (dy === 0 && dx === 0) {
+						continue;
+					}
+					var mx = q[0].x + dx;
+					var my = q[0].y + dy;
+					// make sure we haven't checked it already
+					if (fmap[my] !== undefined && fmap[my][mx] !== undefined) {
+						continue;
+					}
+					// check walkable
+					if (!this.tile_moveable(mx,my)) {
+						continue;
+					}
+					// possible loop through targets here
+					// Passed walkable
+					// add its previous
+					if (fmap[my] === undefined) {
+						fmap[my] = [];
+					}
+					fmap[my][mx] = {px: q[0].x, py: q[0].y};
+					// add to q
+					q.push({x:mx, y:my, depth:q[0].depth+1});
+				}
+			}
+		}
+		q.shift(1);
+	}
+	if (reached) {
+		//make the path
+		var path = [];
+		var px = ex;
+		var py = ey;
+		while (true) {
+			if (px == sx && py == sy) {
+				path.push({x:px, y:py});
+				break;
+			}
+			path.push({x:px, y:py});
+			var next = fmap[py][px];
+			px = next.px;
+			py = next.py;
+		}
+		return path;
+	}
+	return [];
+}
 
 RushNCrush.prototype.ray_cast_clear = function() {
 	// clear all the tiles
@@ -899,7 +1131,7 @@ RushNCrush.prototype.ray_cast = function(px, py, ex, ey) {
 		this.map[sy][sx].lit = true;
 		tt = this.map[sy][sx].tType;
 		if (tt == 1 || tt == 2 || tt == 3) {
-			return;
+			return; // no walk or see
 		}
 
 		if (err > 0) {
